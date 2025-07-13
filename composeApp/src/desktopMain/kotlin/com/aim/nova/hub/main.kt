637221249b6e34e195e112c32ea5961d03473359
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2025-07-13 20:16:46 +0530

    create a fixed sized window initially opened in the bottom right corner.

--- composeApp/src/desktopMain/kotlin/com/aim/nova/hub/main.kt
+++ composeApp/src/desktopMain/kotlin/com/aim/nova/hub/main.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import novahub.composeapp.generated.resources.Res
 import novahub.composeapp.generated.resources.icon
@@ -12,7 +15,8 @@ import org.jetbrains.compose.resources.painterResource
 fun main() = application {
 
     val trayState: TrayState = rememberTrayState()
-    val windowState: WindowState = rememberWindowState(size = DpSize())
+    val windowState: WindowState = rememberWindowState(size = DpSize(400.dp, 660.dp), position = WindowPosition(
+        Alignment.BottomEnd))
     var isWindowVisible by remember { mutableStateOf(true); }
     val notification = rememberNotification("Notification", "Message from MyApp!")
 
@@ -34,12 +38,14 @@ fun main() = application {
             )
         },
         onAction = { isWindowVisible = true }
-
     )
 
     if (isWindowVisible)
         Window(
+            state = windowState,
             title = "Nova Hub",
+            resizable = false,
+            undecorated = false,
             onCloseRequest = { isWindowVisible = false },
         ) { App() }
 
