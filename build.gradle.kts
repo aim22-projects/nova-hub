0f773645141984229addf409c017ab337abf6808
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2025-07-13 23:06:13 +0530

    update (desktopRunHot) build configuration to run a custom clean Gradle script which cleans declared build directories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,21 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
+
+// note: create custom gradle command to clean the build dirs
+tasks.register("cleanCustomBuildDirs") {
+    doFirst {
+        val dirs = listOf(
+            File("build"),
+            File("composeApp/build")
+        )
+        dirs.forEach { dir ->
+            if (dir.exists()) {
+                println("Deleting ${dir.path}")
+                dir.deleteRecursively()
+            } else {
+                println("${dir.path} not found, skipping.")
+            }
+        }
+    }
+}
