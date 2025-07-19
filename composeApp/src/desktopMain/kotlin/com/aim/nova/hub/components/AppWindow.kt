35a215967baa790a89b895d9c851e93e1705af29
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2025-07-19 18:57:14 +0530

    apply custom window border and radius

--- composeApp/src/desktopMain/kotlin/com/aim/nova/hub/components/AppWindow.kt
+++ composeApp/src/desktopMain/kotlin/com/aim/nova/hub/components/AppWindow.kt
@@ -1,11 +1,17 @@
 package com.aim.nova.hub.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -25,18 +31,31 @@ fun AppWindow(
         title = "Nova Hub",
         resizable = false,
         undecorated = true,
-        //  transparent = true,
+        transparent = true,
         onCloseRequest = onClose,
     )
     {
-        Column(Modifier.fillMaxSize().background(Color.White)) {
-            // Custom Title Bar acting as draggable area
-            AppDraggableTopBar(
-                windowState,
-                onCloseRequest = onClose
-            )
-            // Rest of your window content
-            content()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .border(
+                    border = BorderStroke(2.dp, Color.Gray), // custom border
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .clip(RoundedCornerShape(12.dp)) // <--- important to prevent overflow
+                .background(Color.Gray) // background inside border
+                .padding(2.dp)
+        ) {
+            // Your UI here
+            Column(Modifier.fillMaxSize().background(Color.White)) {
+                // Custom Title Bar acting as draggable area
+                AppDraggableTopBar(
+                    windowState,
+                    onCloseRequest = onClose
+                )
+                // Rest of your window content
+                content()
+            }
         }
     }
 }
