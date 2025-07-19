0d8719efaecd06735a89b300d625592f89d735d3
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2025-07-19 23:02:12 +0530

    create custom app theme

---
+++ composeApp/src/desktopMain/kotlin/com/aim/nova/hub/components/AppTheme.kt
@@ -0,0 +1,37 @@
+package com.aim.nova.hub.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+val LightColors = lightColorScheme(
+    primary = Color(0xFF6200EE),
+    onPrimary = Color.White,
+    background = Color(0xFFF0F0F0),
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black,
+)
+
+val DarkColors = darkColorScheme(
+    primary = Color(0xFFBB86FC),
+    onPrimary = Color.Black,
+    background = Color(0xFF121212),
+    onBackground = Color.White,
+    surface = Color(0xFF1E1E1E),
+    onSurface = Color.White,
+)
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    val colors = if (isSystemInDarkTheme()) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
